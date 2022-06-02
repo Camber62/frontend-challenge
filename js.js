ed42027807aada0b4af2d23afaf3d10c12475e52
hp-pc <Camber62@mail.ru>
2022-06-02 11:54:58 +0300

    work w

--- js.js
+++ js.js
@@ -19,29 +19,29 @@ headers:{
 
 
 
-const heart = document.querySelectorAll('.heart')
 const heartIMG = document.querySelectorAll('.heart img')
 
 
 
 
-function like(event) {
-for(let i = 0;i<heartIMG.length;i++) {
-    let eventTarget = event.target.alt
-    if (eventTarget === "img") {
-        heartIMG[i].src = 'icons/Like.svg'
-        heartIMG[i].alt = 'like'
-    } else if (eventTarget === "like") {
-        heartIMG[i].src = 'icons/Vector.svg'
-        heartIMG[i].alt = 'img'
-    }
-    console.log(heartIMG.length)
-}
-}
+// function like(event) {
+// for(let i = 0;i<heartIMG.length;i++) {
+//
+//     console.log(heartIMG.length)
+// }
+// }
 
-for (let i = 0;i<heart.length;i++) {
-    addEventListener('click', function (event) {
-        like(event)
+for (let i = 0;i<heartIMG.length;i++) {
+    heartIMG[i].addEventListener('click', function (event) {
+        let eventTarget = event.target.alt
+        if (eventTarget === "img") {
+            heartIMG[i].src = 'icons/Like.svg'
+            heartIMG[i].alt = 'like'
+        } else if (eventTarget === "like") {
+            heartIMG[i].src = 'icons/Vector.svg'
+            heartIMG[i].alt = 'img'
+        }
+
     })
 }
 
